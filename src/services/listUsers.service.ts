98465f412c0c8b90e308aa488fe7276be8ad78d5
JoabeConrado19 <joabe.conrado19@gmail.com>
2022-12-20 08:33:07 -0400

    fix patch

--- src/services/listUsers.service.ts
+++ src/services/listUsers.service.ts
@@ -1,10 +1,18 @@
 import AppDataSource from "../data-source"
 import { User } from "../entities/user.entity"
+import { listUsersReturnedData } from "../serializers/users.serializers"
 
 const listUsersService = async () => {
     const userRepository = AppDataSource.getRepository(User)
 
     const users = await userRepository.find()
+
+    const returnedUser: any = await listUsersReturnedData.validate(users, {
+        stripUnknown: true
+      })
+    
+      return returnedUser
+
     return users
 }
 
